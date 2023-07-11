37468dabdf3c25735ba77c9327d8f0b930e3ade4
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-07-11 22:41:41 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "olunapp-dev-100063",
-  url: "https://olunapp-dev-100063.botics.co",
-  api: "https://olunapp-dev-100063.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "olunapp-dev-100063",
+  "url": "https://olunapp-dev-100063.botics.co",
+  "api": "https://olunapp-dev-100063.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- store/olunappdevAPI/api.js
+++ store/olunappdevAPI/api.js
@@ -1,52 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const olunappdevAPI = axios.create({
   baseURL: "https://olunapp-dev-100063.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return olunappdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return olunappdevAPI.post(`/api/v1/login/`, payload)
+  return olunappdevAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return olunappdevAPI.post(`/api/v1/signup/`, payload)
+  return olunappdevAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/login/`, payload)
+  return olunappdevAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return olunappdevAPI.get(`/rest-auth/logout/`)
+  return olunappdevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/logout/`)
+  return olunappdevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/password/change/`, payload)
+  return olunappdevAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/password/reset/`, payload)
+  return olunappdevAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return olunappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/registration/`, payload)
+  return olunappdevAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return olunappdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return olunappdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return olunappdevAPI.get(`/rest-auth/user/`)
+  return olunappdevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return olunappdevAPI.put(`/rest-auth/user/`, payload)
+  return olunappdevAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return olunappdevAPI.patch(`/rest-auth/user/`, payload)
+  return olunappdevAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -62,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
