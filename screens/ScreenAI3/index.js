09c8c33779bf7f4d9cfc710934855199ee19c000
Ravinder QA Lead <ravinder.y@crowdbotics.com>
2023-07-11 22:44:26 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
+import { api_v1_login_create } from "../../store/olunappdevAPI/authTokens.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 
 const LoginScreen = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(api_v1_login_create({
+      user: "username",
+      pass: "password"
+    }));
+  }, []);
   return <SafeAreaView style={styles.container}>
       <View style={styles.logoContainer}>
         <Text style={styles.logoText}>My App</Text>
@@ -19,40 +29,40 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
   },
   logoContainer: {
     marginBottom: 50
   },
   logoText: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#333'
+    fontWeight: "bold",
+    color: "#333"
   },
   formContainer: {
-    width: '80%'
+    width: "80%"
   },
   input: {
     height: 50,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20
   },
   button: {
     height: 50,
-    backgroundColor: '#333',
+    backgroundColor: "#333",
     borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   buttonText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff'
+    fontWeight: "bold",
+    color: "#fff"
   }
 });
 export default LoginScreen;
